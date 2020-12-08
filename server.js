04e886a16a2e2343b4d1b020d072bd28a8202c38
Noor Massarwe <noor_19771@outlook.com>
2020-12-08 14:31:40 +0200

    first push

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const mongoose = require('mongoose')
+//we need to make DB in atlas
+mongoose.connect("mongodb://localhost/", {useNewUrlParser: true, useUnifiedTopology: true})
+
+
+const api = require('./server/route/api')
+
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use('/',api)
+
+const port = 8080
+app.listen(port, function () {
+    console.log(`server runs on port : ${port}`)
+
+})
