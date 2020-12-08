0d4867a0ca2caf701b9fbc2966cfb3b19e972a18
Aysam Salalha <aysam.s.1998@hotmail.com>
2020-12-08 14:33:28 +0200

    created APIManager Sign Up POST METHOD and User Class Created

---
+++ dist/User.js
@@ -0,0 +1,14 @@
+class User
+{
+    constructor(username,password,name,skills,rank,challenges)
+    {
+        this.username = username
+        this.password = password
+        this.name = name
+        this.skills = skills
+        this.rank = rank
+        this.challenges = challenges
+    }
+
+
+}
