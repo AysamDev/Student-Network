69359a19ee33d2b048d87ed241a5131916c495dc
Aysam Salalha <aysam.s.1998@hotmail.com>
2020-12-09 17:13:16 +0200

    added Challenge POST METHOD to the APIManager

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,69 +5,7 @@ const mongoose = require('mongoose');
 const User = require('../models/User')
 const Challenge = require('../models/Challenge')
 
-const callSave = async function()
-{
-//1
-let ch = await axios.get(`https://www.codewars.com/api/v1/code-challenges/valid-braces?access_key=Ka6QAn4J3s9GvrdE4vtw`)
-let chall = new Challenge(
-    {
-        slug: ch.data.slug,
-        url: ch.data.url,
-        skill: "java" 
-    }
-)
-chall.save()
-//2
- ch = await axios.get(`https://www.codewars.com/api/v1/code-challenges/sum-of-digits-slash-digital-root?access_key=Ka6QAn4J3s9GvrdE4vtw`)
- chall = new Challenge(
-    {
-        slug: ch.data.slug,
-        url: ch.data.url,
-        skill: "javascript" 
-    }
-)
-chall.save()
-//3
-ch = await axios.get(`https://www.codewars.com/api/v1/code-challenges/list-filtering?access_key=Ka6QAn4J3s9GvrdE4vtw`)
-chall = new Challenge(
-   {
-       slug: ch.data.slug,
-       url: ch.data.url,
-       skill: "javascript" 
-   }
-)
-//4
-ch = await axios.get(`https://www.codewars.com/api/v1/code-challenges/who-likes-it?access_key=Ka6QAn4J3s9GvrdE4vtw`)
-chall = new Challenge(
-   {
-       slug: ch.data.slug,
-       url: ch.data.url,
-       skill: "javascript" 
-   }
-)
-chall.save()
-//5
-ch = await axios.get(`https://www.codewars.com/api/v1/code-challenges/friend-or-foe?access_key=Ka6QAn4J3s9GvrdE4vtw`)
-chall = new Challenge(
-   {
-       slug: ch.data.slug,
-       url: ch.data.url,
-       skill: "javascript" 
-   }
-)
-chall.save()
-//6
-ch = await axios.get(`https://www.codewars.com/api/v1/code-challenges/multiply?access_key=Ka6QAn4J3s9GvrdE4vtw`)
-chall = new Challenge(
-   {
-       slug: ch.data.slug,
-       url: ch.data.url,
-       skill: "python" 
-   }
-)
-chall.save()
-}
-callSave()
+
 router.post('/userSignUp', async function (req, res) {
     const Username = req.body.username
     const data = await axios.get(`https://www.codewars.com/api/v1/users/${Username}?access_key=Ka6QAn4J3s9GvrdE4vtw`)
