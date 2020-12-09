3ca8f3a32e68090499df9e4fca6379982bcd583b
Noor Massarwe <noor_19771@outlook.com>
2020-12-09 17:16:05 +0200

    api routs change

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,71 +3,9 @@ const router = express.Router()
 const axios = require('axios');
 const mongoose = require('mongoose');
 const User = require('../models/User')
-const Challenge = require('../models/Challenge')
+const Challenge = require('../models/Challenge');
+const { SSL_OP_SSLREF2_REUSE_CERT_TYPE_BUG } = require('constants');
 
-const callSave = async function()
-{
-//1
-let ch = await axios.get(`https://www.codewars.com/api/v1/code-challenges/valid-braces?access_key=Ka6QAn4J3s9GvrdE4vtw`)
-let chall = new Challenge(
-    {
-        slug: ch.data.slug,
-        url: ch.data.url,
-        skill: "java" 
-    }
-)
-chall.save()
-//2
- ch = await axios.get(`https://www.codewars.com/api/v1/code-challenges/sum-of-digits-slash-digital-root?access_key=Ka6QAn4J3s9GvrdE4vtw`)
- chall = new Challenge(
-    {
-        slug: ch.data.slug,
-        url: ch.data.url,
-        skill: "javascript" 
-    }
-)
-chall.save()
-//3
-ch = await axios.get(`https://www.codewars.com/api/v1/code-challenges/list-filtering?access_key=Ka6QAn4J3s9GvrdE4vtw`)
-chall = new Challenge(
-   {
-       slug: ch.data.slug,
-       url: ch.data.url,
-       skill: "javascript" 
-   }
-)
-//4
-ch = await axios.get(`https://www.codewars.com/api/v1/code-challenges/who-likes-it?access_key=Ka6QAn4J3s9GvrdE4vtw`)
-chall = new Challenge(
-   {
-       slug: ch.data.slug,
-       url: ch.data.url,
-       skill: "javascript" 
-   }
-)
-chall.save()
-//5
-ch = await axios.get(`https://www.codewars.com/api/v1/code-challenges/friend-or-foe?access_key=Ka6QAn4J3s9GvrdE4vtw`)
-chall = new Challenge(
-   {
-       slug: ch.data.slug,
-       url: ch.data.url,
-       skill: "javascript" 
-   }
-)
-chall.save()
-//6
-ch = await axios.get(`https://www.codewars.com/api/v1/code-challenges/multiply?access_key=Ka6QAn4J3s9GvrdE4vtw`)
-chall = new Challenge(
-   {
-       slug: ch.data.slug,
-       url: ch.data.url,
-       skill: "python" 
-   }
-)
-chall.save()
-}
-callSave()
 router.post('/userSignUp', async function (req, res) {
     const Username = req.body.username
     const data = await axios.get(`https://www.codewars.com/api/v1/users/${Username}?access_key=Ka6QAn4J3s9GvrdE4vtw`)
@@ -88,16 +26,10 @@ router.post('/userSignUp', async function (req, res) {
 })
 
 
-router.get('/challenge/:type', async function (req, res) {
-    const type = req.params.type
-    axios.get(`https://www.codewars.com/api/v1/code-challenges/${type}?access_key=Ka6QAn4J3s9GvrdE4vtw`)
-        .then(function (response) {
-            // handle success
-            res.send(response.data)
-        })
-        .catch(function (error) {
-            res.send(error)
-        })
+router.post('/challenges/', async function (req, res) {
+    const type = req.body.skill
+    const found= await Challenge.find({skill:type})
+    res.send(found)
 })
 
 router.post('/userSignIn/', async function (req, res) {
@@ -121,7 +53,7 @@ router.post('/userSignIn/', async function (req, res) {
     res.send(user)
 })
 //update 
-router.post('/addChallenge',async function(req,res){
+/*router.post('/addChallenge',async function(req,res){
     const challenge = req.body.challenge
     const user = req.body.user
     User.findOneAndUpdate({userName:user},{ $push: { challenges: challenge  } }, function(err,data){
@@ -129,6 +61,6 @@ router.post('/addChallenge',async function(req,res){
             res.send(err)
         res.end()    
     })
-})
+})*/
 
 module.exports = router
